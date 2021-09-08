aa0ece107a771395536cf821cdb68d6bbc39db5e
wangshixiong <shi.xiong@tenxcloud.com>
2021-09-08 18:24:04 +0800

    加了个eureka-server的model, 以及去掉sdk里面的.cluster.local 后缀

---
+++ tdsf/eureka-demo/eureka-server/src/main/java/com/tenxcloud/eureka/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.tenxcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangshixiong
+ * @date 2021-09-03 下午4:47
+ */
+@EnableDiscoveryClient
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
