c44fd1b51b5c24315ad552ae692abd17ffa4743e
zhang.shuo5 <shuo.zhang@tenxcloud.com>
2022-01-24 16:53:31 +0800

    tdsf530

--- tdsf/nacos-demo/springcloud-consumer/src/main/java/com/tenxcloud/springcloud/consumer/service/HelloService.java
+++ tdsf/nacos-demo/springcloud-consumer/src/main/java/com/tenxcloud/springcloud/consumer/service/HelloService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author wangshixiong
  * @date 2020-12-15 5:41 下午
  */
-@FeignClient(value = "springcloud-demo-provider")
+@FeignClient(value = "springcloud-provider")
 public interface HelloService {
 
     @GetMapping(value = "hello")
